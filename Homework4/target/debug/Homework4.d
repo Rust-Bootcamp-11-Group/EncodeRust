1895717200e8766c5e008699eb4dfed29152a541
German Kosach <fuerstein@yandex.ru>
2024-08-08 17:36:11 +0300

    feat(): init

---
+++ Homework4/target/debug/Homework4.d
@@ -0,0 +1,1 @@
+D:\Programming\Rust\BootcampEncode\Homework4\target\debug\Homework4.exe: D:\Programming\Rust\BootcampEncode\Homework4\src\main.rs
---
+++ Homework4/target/debug/deps/Homework4.d
@@ -0,0 +1,5 @@
+D:\Programming\Rust\BootcampEncode\Homework4\target\debug\deps\Homework4.exe: src/main.rs
+
+D:\Programming\Rust\BootcampEncode\Homework4\target\debug\deps\Homework4.d: src/main.rs
+
+src/main.rs:
